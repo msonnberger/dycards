ceba987357204c75771f990c5ec5e2406ca5a28e
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2022-01-31 17:10:05 +0100

    add endscreen

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -14,6 +14,19 @@ class FlashcardsController < ApplicationController
     @flashcard = Flashcard.find(params[:id])
   end
 
+  def endscreen
+    @flashcard = Flashcard.find(params[:id])
+    stack = @flashcard.stack
+    #all = stack.flashcards
+    total_count = stack.flashcards.count.to_f
+    @percentages = Hash.new
+    @percentages['open_answer'] = OpenAnswer.where(stack_id: stack.id).count / total_count
+    @percentages['multiple_choice'] = MultipleChoice.where(stack_id: stack.id).count / total_count
+    @percentages['single_choice'] = SingleChoice.where(stack_id: stack.id).count / total_count
+    @percentages['true_false'] = TrueFalse.where(stack_id: stack.id).count / total_count
+    puts @percentages
+  end
+
   def edit
   end
 
