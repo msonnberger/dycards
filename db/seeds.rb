4c7eef5bc75451d2927fb622a9960a457eae78b2
Maximilian Hajek <mhajek.mmt-b2020@fh-salzburg.ac.at>
2022-02-01 09:53:40 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,13 @@
 
 test_user = User.create(username: 'test', password: 'test', password_confirmation: 'test')
 stack = test_user.stacks.create(title: 'Stack 1')
-open_answer = OpenAnswer.create(
+
+OpenAnswer.create(
   question: 'Open Question?',
   answer: 'Open Question!', stack_id: stack.id
 )
 
-multiple_choice = MultipleChoice.create(
+MultipleChoice.create(
   question: 'MC Question?',
   option1: 'MC Option1!',
   option2: 'MC Option2!',
@@ -26,7 +27,7 @@ multiple_choice = MultipleChoice.create(
   stack_id: stack.id
 )
 
-single_choice = SingleChoice.create(
+SingleChoice.create(
   question: 'SC Question?',
   option1: 'SC Option1!',
   option2: 'SC Option2!',
@@ -36,7 +37,7 @@ single_choice = SingleChoice.create(
   stack_id: stack.id
 )
 
-true_false = TrueFalse.create(
+TrueFalse.create(
   question: 'True False Question?',
   correct: true,
   stack_id: stack.id
