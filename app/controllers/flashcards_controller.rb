ee2bc48b367614290693d951b517339d13411d00
Martin Sonnberger <msonnberger.mmt-b2020@fh-salzburg.ac.at>
2022-02-01 11:56:00 +0100

    save correct answers to session

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -12,11 +12,26 @@ class FlashcardsController < ApplicationController
 
   def show
     @flashcard = Flashcard.find(params[:id])
+    if !session[:open_answer]
+      session[:open_answer] = 0
+      session[:multiple_choice] = 0
+      session[:single_choice] = 0
+      session[:true_false] = 0
+    end
   end
 
   def next
     @flashcard = Flashcard.find(params[:id])
     current_user.completed_flashcards.create(flashcard_id: @flashcard.id)
+
+    types = {
+      'OpenAnswer' => :open_answer,
+      'MultipleChoice' => :multiple_choice,
+      'SingleChoice' => :single_choice,
+      'TrueFalse' => :true_false
+    }
+
+    session[types[@flashcard.type]] += 1 if params[:correct] == 'true'
 
     if @flashcard.next
       redirect_to flashcard_path(@flashcard.next)
@@ -35,14 +50,23 @@ class FlashcardsController < ApplicationController
   def destroy
   end
 
+  def flashcard_params
+    params.permit :id, :stack_id, :correct
+  end
+
+  helper_method :flashcard_params
+
   private
     def endscreen_data
       stack = @flashcard.stack
-      total_count = stack.flashcards.count.to_f
+      open_answer_count = stack.flashcards.where(type: 'OpenAnswer').count.to_f
+      multiple_choice_count = stack.flashcards.where(type: 'MultipleChoice').count.to_f
+      single_choice_count = stack.flashcards.where(type: 'SingleChoice').count.to_f
+      true_false_count = stack.flashcards.where(type: 'TrueFalse').count.to_f
       @percentages = {}
-      @percentages['open_answer'] = OpenAnswer.where(stack_id: stack.id).count / total_count
-      @percentages['multiple_choice'] = MultipleChoice.where(stack_id: stack.id).count / total_count
-      @percentages['single_choice'] = SingleChoice.where(stack_id: stack.id).count / total_count
-      @percentages['true_false'] = TrueFalse.where(stack_id: stack.id).count / total_count
+      @percentages[:open_answer] = session[:open_answer] / open_answer_count * 100
+      @percentages[:multiple_choice] = session[:multiple_choice] / multiple_choice_count * 100
+      @percentages[:single_choice] = session[:single_choice] / single_choice_count * 100
+      @percentages[:true_false] = session[:true_false] / true_false_count * 100
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
 
   resources :stacks
   resources :flashcards
-  get '/flashcards/:id/next', to: 'flashcards#next'
+  get '/flashcards/:id/next', to: 'flashcards#next', as: :next_card
 end
