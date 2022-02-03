477b8b116ac5771dbd700e359907ac840a5908f5
Maximilian Hajek <mhajek.mmt-b2020@fh-salzburg.ac.at>
2022-02-03 11:28:36 +0100

    Add stats styling

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,12 +4,19 @@ class StatsController < ApplicationController
     total = completed.all.count
 
     if total > 0
+      multiple_choice = completed.where(type: 'MultipleChoice').count
+      open_answer = completed.where(type: 'OpenAnswer').count
+      single_choice = completed.where(type: 'SingleChoice').count
+      true_false = completed.where(type: 'TrueFalse').count
+
+      values = Array.[](multiple_choice, open_answer, single_choice, true_false)
+
       @stats = {
         total: total,
-        multiple_choice: completed.where(type: 'MultipleChoice').count / total.to_f * 100,
-        open_answer: completed.where(type: 'OpenAnswer').count / total.to_f * 100,
-        single_choice: completed.where(type: 'SingleChoice').count / total.to_f * 100,
-        true_false: completed.where(type: 'TrueFalse').count / total.to_f * 100,
+        open_answer: (100 / values.max().to_f * open_answer.to_f).round(),
+        multiple_choice: (100 / values.max().to_f * multiple_choice.to_f).round(),
+        single_choice: (100 / values.max().to_f * single_choice.to_f).round(),
+        true_false: (100 / values.max().to_f * true_false.to_f).round(),
       }
     end
   end
