e95ac490ef78a86683b91b21bf24b263af69a48f
Maximilian Hajek <mhajek.mmt-b2020@fh-salzburg.ac.at>
2022-02-04 15:46:27 +0100

    Hotfix

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,12 +7,33 @@ class StatsController < ApplicationController
       counts.transform_values!(&:to_f)
       maximum = counts.values.max.to_f
 
+      open_answer = 0
+      unless counts['OpenAnswer'].nil? || counts['OpenAnswer'] == 0
+        open_answer = 100 / maximum * counts['OpenAnswer']
+      end
+
+      multiple_choice = 0
+      unless counts['MultipleChoice'].nil? || counts['MultipleChoice'] == 0
+        multiple_choice = 100 / maximum * counts['MultipleChoice']
+      end
+
+      single_choice = 0
+      unless counts['SingleChoice'].nil? || counts['SingleChoice'] == 0
+        single_choice = 100 / maximum * counts['SingleChoice']
+      end
+
+      true_false = 0
+      unless counts['TrueFalse'].nil? || counts['TrueFalse'] == 0
+        true_false = 100 / maximum * counts['TrueFalse']
+      end
+
+
       @stats = {
         total: counts.values.sum,
-        open_answer: 100 / maximum * counts['OpenAnswer'],
-        multiple_choice: 100 / maximum * counts['MultipleChoice'],
-        single_choice: 100 / maximum * counts['SingleChoice'],
-        true_false: 100 / maximum * counts['TrueFalse'],
+        open_answer: open_answer,
+        multiple_choice: multiple_choice,
+        single_choice: single_choice,
+        true_false: true_false,
       }
 
       @stats.transform_values!(&:round)
